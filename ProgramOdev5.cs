487d411b07d45eb2db4f04407edfb50a3371ca11
Kübra Satılmış <kmrt3014@gmail.com>
2024-03-06 21:32:18 +0300

    Add files via upload

---
+++ ProgramOdev5.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleAppOdev5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan sayı girmesini istedik
+            Console.Write("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //IsPerfectNumber fonksiyonundan dönen değer true ise bilgiyi yazdırdık
+            if (IsPerfectNumber(number))
+            {
+                Console.WriteLine($"{number} is a perfect  number !");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a perfect number !");
+            }
+
+
+        }
+
+        //Girilen sayının mükemmel olup olmadığının kontrolünü bu fonksiyon içinde yaptık
+        static bool IsPerfectNumber(int num)
+        {
+            int total = 0;
+            //Bu döngü i'den başlayarak sayının bir önceki değerine kadar olan sayıları kontrol edip pozitif bölenleri kontrol eder.
+            for (int i = 1; i < num; i++)
+            {
+                //eğer num parametresin i ile bölümünden kalan 0 ise i değeri num parametresinin bir bölenidir. Bu yüzden total'e i değerini ekledik
+                if (num % i == 0)
+                {
+                    total += i;
+                }
+            }
+
+            return total == num;
+
+        }
+    }
+}
---
+++ ProgramOdev7.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleAppOdev7
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            
+            Console.Write("Lütfen öğrenci sayısını giriniz: ");
+            int studentNumber = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 1; i <= studentNumber; i++)
+            {
+                Console.Write("Lütfen öğrenci adını giriniz: ");
+                string studentName = Console.ReadLine();
+
+                Console.Write("Lütfen öğreci soyadını giriniz: ");
+                string studentSurname = Console.ReadLine();
+
+                Console.Write("Lütfen 1. Sınav notunu giriniz: ");
+                int s1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Lütfen 2. sınav notunu giriniz: ");
+                int s2 = int.Parse(Console.ReadLine());
+
+                Console.Write("Lütfen 3. Sınav notunu giriniz: ");
+                int s3 = int.Parse(Console.ReadLine());
+
+
+                double gradeAverage = (s1 + s2 + s3) / 3;
+                if (gradeAverage > 100)
+                {
+                    Console.WriteLine("Lütfen sınav notlarını kontrol ediniz. Sınav Notları İki Basamaklı Olmalıdır !");
+                }
+                else
+                {
+                    Console.WriteLine($"Öğrencinin not ortalaması : {gradeAverage}");
+                }
+                
+            }
+
+            
+
+
+
+
+        }
+    }
+}
